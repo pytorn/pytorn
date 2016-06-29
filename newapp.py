89e4df3adb0376634c0194f548e4dfdb48dac64f
Shubhodeep Mukherjee <shubhodeep9@gmail.com>
2016-06-29 10:05:14 +0530

    Few bugs

--- newapp.py
+++ newapp.py
@@ -134,7 +134,7 @@ application = Application(route, **settings)
 
 if __name__ == "__main__":
 	server = HTTPServer(application)
-	server.listen(torn.Port)
+	server.listen(int(torn.Port))
 	IOLoop.current().start()
 
 					""")
