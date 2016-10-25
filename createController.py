530eb5f9f061c832ed1339df48167a3d3373e28a
Ashwini Purohit <ashwinipurohit@yahoo.in>
2016-10-25 12:03:15 +0530

    current directory as local and helper comments

--- createController.py
+++ createController.py
@@ -2,16 +2,21 @@ import os
 from colors import bcolors
 class createNewController():
 	NAME = ''
-	CURRENT_DIR = os.getcwd()
-	INFO = bcolors.OKBLUE+'INFO:'+bcolors.ENDC
-	SUCCESS = bcolors.OKGREEN+'SUCCESS:'+bcolors.ENDC
 	def __init__(self,name):
 		self.NAME = name+'.py'
 		self.create()
 
 	def create(self):
-		path = self.CURRENT_DIR+'/controllers/'
+		CURRENT_DIR = os.getcwd()
+		#For apis and webapps, controller directory is set to controllers
+		path = CURRENT_DIR+'/controllers/'
 		os.chdir(path)
+		'''
+		Initialised controllerData, that will help in creating preset data
+		like importing modules etc
+		self.NAME[:len(self.NAME)-3] is used to remove the extension .py from the file
+		while printing the message on command line.
+		'''
 		controllerData = "#"+self.NAME[:len(self.NAME)-3]+" Controller created"
 		conf = open(self.NAME,'w')
 		conf.write(controllerData)
