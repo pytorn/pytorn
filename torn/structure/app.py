87799fcb81d578824e33579f8e243d13c8c26d6c
Shubhodeep Mukherjee <shubhodeep9@gmail.com>
2017-09-06 02:16:50 +0530

    More additions

--- torn/structure/app.py
+++ torn/structure/app.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from torn import
+from torn.api.app import TornadoApp
 
 def run():
-	
+	app = TornadoApp(port = 8080)
+	app.run()
 
 if __name__ == '__main__':
 	run()
