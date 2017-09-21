cb28be5b78d15bf54f0e2689bbe921442d69d59a
Shubhodeep Mukherjee <shubhodeep9@gmail.com>
2017-09-21 19:07:34 +0530

    Cli

--- torn/__init__.py
+++ torn/__init__.py
@@ -3,5 +3,17 @@
 from argparse import ArgumentParser, RawDescriptionHelpFormatter
 
 def cli():
-    parser = ArgumentParser()
-    parser.parse_args()
+	epilog = ('The actions are:\n'
+		'\tnew\t\tCreate a Tornado Application\n'
+		'\trun\t\trun the app and start a Web server for development\n'
+		'\tapi\t\tcreate an API tornado application\n'
+		'\tversion\t\treturns the current version of torn\n')
+	parser = ArgumentParser(prog = 'torn',
+    						description='A full fledged Tornado MVC framework [In-progress]',
+    						formatter_class=RawDescriptionHelpFormatter,
+    						epilog = epilog,
+    						usage='%(prog)s [action [arguments]]')
+	parser.add_argument('action', nargs='*', default = ['new', 'api', 'run'] ,help='Action to be performed with the application')
+	parser.parse_args()
+
+
