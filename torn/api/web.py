4a139e38d4064fd885d2beed260ba1f894195595
Shubhodeep Mukherjee <shubhodeep9@gmail.com>
2017-10-18 14:53:54 +0530

    Custom Request and Response classes

---
+++ torn/api/web.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from tornado.httpclient import HTTPRequest, HTTPResponse
+
+class Request(HTTPRequest):
+    pass
+
+class Response(HTTPResponse):
+    pass
