54b4082899253df056d54b165233e211b7d02bd9
Shubhodeep Mukherjee <shubhodeep9@gmail.com>
2017-11-22 00:13:36 +0530

    proper modification to docstring

--- torn/plugins/log.py
+++ torn/plugins/log.py
@@ -1,27 +1,51 @@
-"""Log consists of functions required to display a log on the terminal."""
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+"""Log consists of functions required to display a log on the terminal."""
 
 from __future__ import print_function
 from datetime import datetime
 import torn.plugins.colors
 
 def warning(message):
-    """Definition for displaying a warning."""
+    """Display Warning.
+
+    Method prints the warning message, message being given
+    as an input.
+
+    Arguments:
+        message {string} -- The message to be displayed.
+    """
+
     now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     print((now + ' [' + torn.plugins.colors.WARNING)
           ('WARNING' + torn.plugins.colors.ENDC + '] ')
           (message))
 
 def info(message):
-    """Definition for displaying an information."""
+    """Display Information.
+
+    Method prints the information message, message being given
+    as an input.
+
+    Arguments:
+        message {string} -- The message to be displayed.
+    """
+
     now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     print((now + ' [' + torn.plugins.colors.OKBLUE)
           ('INFO' + torn.plugins.colors.ENDC + '] ')
           (message))
 
 def error(message):
-    """Definition for displaying an error."""
+    """Display Error.
+
+    Method prints the error message, message being given
+    as an input.
+
+    Arguments:
+        message {string} -- The message to be displayed.
+    """
     now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     print((now + ' [' + torn.plugins.colors.FAIL)
           ('ERROR' + torn.plugins.colors.ENDC + '] ')
