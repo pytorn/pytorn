31bfeccb7ecee6a843afcd4f014c7a2ed7d308da
Shubhodeep Mukherjee <shubhodeep9@gmail.com>
2017-11-21 15:13:47 +0530

    linting to __init__

--- torn/__init__.py
+++ torn/__init__.py
@@ -1,19 +1,23 @@
+"""This module initiates the command line for torn"""
+
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from argparse import ArgumentParser, RawDescriptionHelpFormatter
 
 def cli():
+    """Function for cli"""
     epilog = ('The actions are:\n'
-        '\tnew\t\tCreate a Tornado Application\n'
-        '\trun\t\trun the app and start a Web server for development\n'
-        '\tapi\t\tcreate an API tornado application\n'
-        '\tversion\t\treturns the current version of torn\n')
-    parser = ArgumentParser(prog = 'torn',
+              '\tnew\t\tCreate a Tornado Application\n'
+              '\trun\t\trun the app and start a Web server for development\n'
+              '\tapi\t\tcreate an API tornado application\n'
+              '\tversion\t\treturns the current version of torn\n')
+    parser = ArgumentParser(prog='torn',
                             description='A full fledged Tornado MVC framework [In-progress]',
                             formatter_class=RawDescriptionHelpFormatter,
-                            epilog = epilog,
+                            epilog=epilog,
                             usage='%(prog)s [action [arguments]]')
-    parser.add_argument('action', nargs='*', default = ['new', 'api', 'run'] ,help='Action to be performed with the application')
+    parser.add_argument('action',
+                        nargs='*',
+                        default=['new', 'api', 'run'],
+                        help='Action to be performed with the application')
     parser.parse_args()
-
-
