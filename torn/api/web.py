8e0df31e0bb24d675f26203a20f16e1a92659c43
Shubhodeep Mukherjee <shubhodeep9@gmail.com>
2017-11-23 22:43:15 +0530

    few changes

--- torn/api/web.py
+++ torn/api/web.py
@@ -4,7 +4,8 @@
 from tornado.httpclient import HTTPRequest, HTTPResponse
 
 class Request(HTTPRequest):
+    """Request class inherits tornado.httpclient.HTTPRequest class."""
     pass
-
 class Response(HTTPResponse):
+    """Request class inherits tornado.httpclient.HTTPResponse class."""
     pass
---
+++ torn/plugins/__init__.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Module provides helper classes and methods for the framework."""
