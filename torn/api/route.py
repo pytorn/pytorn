92e492551f06978d35ac07e96ec77f11944ed801
Shubhodeep Mukherjee <shubhodeep9@gmail.com>
2018-07-24 19:06:40 +0530

    default and regex to dict debug

--- torn/api/route.py
+++ torn/api/route.py
@@ -9,7 +9,7 @@ class Routing:
     def __init__(self):
         self.routes = {}
 
-    def _add(self, method, uri, name, controller, defaults=[], regex=[]):
+    def _add(self, method, uri, name, controller, defaults={}, regex={}):
         # replace variables with regex if exists or with default regex
         uri = torn.plugins.app.uri_creator(uri, regex, defaults)
         self.routes[name] = {
@@ -21,20 +21,20 @@ class Routing:
         else:
             self.routes[uri]['method'] = [method]
 
-    def get(self, uri, name, controller, defaults=[], regex=[]):
-        self._add('GET', uri, name, controller, defaults=[], regex=[])
+    def get(self, uri, name, controller, defaults={}, regex={}):
+        self._add('GET', uri, name, controller, defaults={}, regex={})
 
-    def post(self, uri, name, controller, defaults=[], regex=[]):
-        self._add('POST', uri, name, controller, defaults=[], regex=[])
+    def post(self, uri, name, controller, defaults={}, regex={}):
+        self._add('POST', uri, name, controller, defaults={}, regex={})
 
-    def put(self, uri, name, controller, defaults=[], regex=[]):
-        self._add('PUT', uri, name, controller, defaults=[], regex=[])
+    def put(self, uri, name, controller, defaults={}, regex={}):
+        self._add('PUT', uri, name, controller, defaults={}, regex={})
 
-    def patch(self, uri, name, controller, defaults=[], regex=[]):
-        self._add('PATCH', uri, name, controller, defaults=[], regex=[])
+    def patch(self, uri, name, controller, defaults={}, regex={}):
+        self._add('PATCH', uri, name, controller, defaults={}, regex={})
 
-    def delete(self, uri, name, controller, defaults=[], regex=[]):
-        self._add('DELETE', uri, name, controller, defaults=[], regex=[])
+    def delete(self, uri, name, controller, defaults={}, regex={}):
+        self._add('DELETE', uri, name, controller, defaults={}, regex={})
 
 class Router(tornado.routing.Router):
     def __init__(self, app, route = Routing()):
